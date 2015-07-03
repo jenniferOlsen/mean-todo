093867a68d18c559b6de2d3383b6a2f31c7c7b18
Jenny Olsen <jenny.a.olsen@gmail.com>
2015-07-03 12:18:25 -0700

    add CRUD functions to api

--- server.js
+++ server.js
@@ -15,6 +15,55 @@ app.use(bodyParser.json());                                     // parse applica
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
 
+// define model
+var Todo = mongoose.model('Todo', {
+  text: String
+});
+
+// routes
+  //api
+  //get all todos
+  app.get('/api/todos', function(req, res) {
+    //fetch from db
+    Todo.find(function(err, todos) {
+      if (err) //if error, send error
+        res.send(err)
+      res.json(todos); //else send todos
+    });
+  });
+
+  //create todos
+  app.post('/api/todos', function(req, res) {
+    Todo.create({
+      text : req.body.text,
+      done : false
+    }, function(err, todo) {
+      if (err)
+        res.send(err);
+      //after creation, get all todos
+      Todo.find(function(err, todos) {
+        if (err)
+          res.send(err)
+        res.json(todos);
+      });
+    });
+  });
+
+  //delete todo
+  app.delete('/api/todos/:todo_id', function(req, res) {
+    Todo.remove({
+      _id : req.params.todo_id
+    }, function(err, todo) {
+      if (err) 
+        res.send(err);
+      Todo.find(function(err, todos) {
+        if (err)
+          res.send(err)
+        res.json(todos);
+      });
+    });
+  });
+
 // listen (start app with node server.js) 
 app.listen(8080);
 console.log("App listening on port 8080");
